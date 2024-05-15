b7b57ff90b19126766bdf96f5a9d07adc6c94731
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-05-15 10:19:30 -0400

    solution for design 2

--- Sample.java
+++ Sample.java
@@ -5,3 +5,56 @@
 
 
 // Your code here along with comments explaining your approach
+
+/*
+    TC - 
+        O(1) for push(), empty() operation
+        O(n) for pop(), peek()
+    Amortized TC - 
+        O(1) for pop(), peek()
+    
+    SC - O(n)
+    Approach - I have used the two stack as mentioned in the problem and checking if it is empty in second stack then push the elements to second stack and peek() and pop()
+*/
+
+class MyQueue {
+
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+    
+    public void push(int x) {
+        in.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return out.pop();
+    }
+    
+    public int peek() {
+        if(out.isEmpty()) {
+            while(!in.isEmpty()) {
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+    
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
