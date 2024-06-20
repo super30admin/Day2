4d1be4a0f3e412cfaedd7e6cf45382192a795875
tnemade <tnemade@uncc.edu>
2024-06-20 18:55:18 -0500

    added Comments
    Added Explanation and time complexity

--- DesignQueueWithStack.cs
+++ DesignQueueWithStack.cs
@@ -1,21 +1,30 @@
+//used two stack one to push the elements and other stack to pop the elements
+//Whlie pushing time complexity is O(1) as element is directly push to stack
+//while pop time complexity is O(1) as we only copy to other stack when that stack is empty
+//so average case time complexity is O(1)
+
+//Total TC: O(1)
+
 public class MyQueue {
     private Stack<int> mainStack;
     private Stack<int> popStack;
-    
     public MyQueue() {
         mainStack= new Stack<int>();
         popStack= new Stack<int>();
     }
     
+    //TC: O(1)
     public void Push(int x) {
         mainStack.Push(x);
     }
     
+    //TC: O(1)
     public int Pop() {
         Peek();  
         return  popStack.Pop();      
     }
     
+    //TC: O(1)
     public int Peek() {
        if(popStack.Count == 0){
             while(mainStack.Count!=0){
@@ -25,6 +34,7 @@ public class MyQueue {
         return popStack.Peek();
     }
     
+    //TC: O(1)
     public bool Empty() {
         if(mainStack.Count == 0 && popStack.Count  == 0){
             return true;
@@ -32,12 +42,3 @@ public class MyQueue {
         return false;
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.Push(x);
- * int param_2 = obj.Pop();
- * int param_3 = obj.Peek();
- * bool param_4 = obj.Empty();
- */
