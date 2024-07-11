a3103088013fea42c587043a3a6a5745447ef331
Samith10 <samithp98@gmail.com>
2024-07-11 10:07:50 -0400

    Done Design2

--- MyQueue.java
+++ MyQueue.java
@@ -1,7 +1,7 @@
 // Time Complexity : O(1)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : No
+// Any problem you faced while coding this : None
 
 
 // Your code here along with comments explaining your approach
