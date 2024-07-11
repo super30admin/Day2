63cf518f43c864bc17a776a6672f49ff49a1e456
Samith10 <samithp98@gmail.com>
2024-07-11 10:06:33 -0400

    Done Design2

---
+++ MyQueue.java
@@ -0,0 +1,56 @@
+// Time Complexity : O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+import java.util.Stack;
+
+class MyQueue {
+    Stack<Integer> in;
+    Stack<Integer> out;
+
+    public void transfer(){
+        while(!in.isEmpty()){
+            out.push(in.pop());
+        }
+    }
+
+    public MyQueue() {
+        in = new Stack<>();
+        out = new Stack<>();
+    }
+    
+    public void push(int x) {
+        in.push(x);
+    }
+    
+    public int pop() {
+        if(out.isEmpty()){
+            transfer();
+        }
+        return out.pop();
+    }
+    
+    public int peek() {
+        if(out.isEmpty()){
+            transfer();
+        }
+        return out.peek();
+    }
+    
+    public boolean empty() {
+        return in.isEmpty() && out.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
