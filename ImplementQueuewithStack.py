8577ef757324a284621023f992700487e6f6a0ac
Pooja Shashidhar <poojts@iu.edu>
2024-07-26 22:47:10 -0400

    Completed Design 2

---
+++ ImplementQueuewithStack.py
@@ -0,0 +1,24 @@
+#TC push is O(1) and Pop is O(1) AVG 
+class MyQueue:
+
+    def __init__(self):
+        self.inStack = []
+        self.outStack = []
+
+    def push(self, x: int) -> None:
+        self.inStack.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.outStack.pop()
+
+    def peek(self) -> int:
+        if len(self.outStack) == 0: 
+            while len(self.inStack) > 0:
+                self.outStack.append(self.inStack.pop())
+        return self.outStack[-1]
+
+
+    def empty(self) -> bool:
+        return len(self.inStack) == 0 and  len(self.outStack)==0
+
