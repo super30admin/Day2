56f0cac325252b5df672884c9d0e6393813099c3
KishoreMunagala <kishorereddy72707@gmail.com>
2024-07-29 18:36:24 -0700

    Design2

--- Sample.java
+++ Sample.java
@@ -1,7 +1,46 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
+// Time Complexity :O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this :
 
 
 // Your code here along with comments explaining your approach
+
+class MyQueue {
+    private Stack<Integer> in;
+    private Stack<Integer> out;
+
+    public MyQueue() 
+    {
+        this.in = new Stack<>();
+        this.out = new Stack<>();   
+    }
+    
+    public void push(int x) 
+    {
+        in.push(x);    
+    }
+    
+    public int pop() 
+    {
+        peek();
+        return out.pop();    
+    }
+    
+    public int peek() 
+    {
+        if(out.isEmpty())
+        {
+            while(!in.isEmpty())
+            {
+                out.push(in.pop());
+            }
+        }
+        return out.peek();
+    }
+    
+    public boolean empty() 
+    {
+        return in.isEmpty() && out.isEmpty();    
+    }
+}
