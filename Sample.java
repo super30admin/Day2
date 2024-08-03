db5fdd0e67291e9411bb9eca68d64a79d4c12155
Boni Preetam Kakarla <130635031+BoniPreetam@users.noreply.github.com>
2024-08-03 12:47:12 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,49 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+import java.util.Stack;
 
+class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
 
-// Your code here along with comments explaining your approach
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+
+// Example usage:
+public class Main {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.push(1); // queue is: [1]
+        myQueue.push(2); // queue is: [1, 2]
+        System.out.println(myQueue.peek()); // return 1
+        System.out.println(myQueue.pop());  // return 1, queue is [2]
+        System.out.println(myQueue.empty());// return false
+    }
+}
