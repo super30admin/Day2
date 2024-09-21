a46c3fbb0d89a5c8068cf6436d488c29f8bf35be
simituli <anupama.tuli@gmail.com>
2024-09-21 14:32:57 -0700

    Update Sample.java - with solution to problem #2

--- Sample.java
+++ Sample.java
@@ -72,11 +72,37 @@ class MyQueue {
  * boolean param_4 = obj.empty();
  */
 
-// Time Complexity : Amortized time complexity O(1)
+// Time Complexity : O(1)
 // Space Complexity : O(n)
 // Did this code successfully run on Leetcode :yes
 // Any problem you faced while coding this : yes - a few compliation errors due to typos.
 
 
 // Your code here along with comments explaining your approach
+class MyHashMap {
+    int[] arr;
+    public MyHashMap() {
+        arr = new int[1000001];
+        Arrays.fill(arr, -1);
+    }
+    
+    public void put(int key, int value) {
+        arr[key]=value;
+    }
+    
+    public int get(int key) {
+        return arr[key];
+    }
+    
+    public void remove(int key) {
+        arr[key]=-1;
+    }
+}
 
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
