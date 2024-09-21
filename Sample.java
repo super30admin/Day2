a9bf31f5e63f921175ea6db80c1181d944c36637
simituli <anupama.tuli@gmail.com>
2024-09-21 14:14:29 -0700

    Update Sample.java with first problem

--- Sample.java
+++ Sample.java
@@ -1,7 +1,82 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : Amortized time complexity O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : yes - a few compliation errors due to typos.
 
 
 // Your code here along with comments explaining your approach
+/* I have created 2 stacks (InSt and OutSt) and I push items in the InSt and pop from the OutSt. 
+    Only when OutSt is empty, do I need to empty the InSt and push all elements to OutSt and then we are ready to pop/ peek from OutSt.
+*/
+class MyQueue {
+    Stack<Integer> InSt;
+    Stack<Integer> OutSt;
+    public MyQueue() {
+        InSt = new Stack<>();
+        OutSt = new Stack<>();
+    }
+    
+    public void push(int x) {
+        InSt.push(x);
+    }
+    
+    public int pop() {
+        if(!OutSt.isEmpty())
+        {
+            return OutSt.pop();
+        }
+        else if (!InSt.isEmpty())
+        {
+            while(!InSt.isEmpty())
+            {
+                OutSt.push(InSt.pop());
+            }
+            return OutSt.pop();
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    
+    public int peek() {
+        if(!OutSt.isEmpty())
+        {
+            return OutSt.peek();
+        }
+        else if (!InSt.isEmpty())
+        {
+            while(!InSt.isEmpty())
+            {
+                OutSt.push(InSt.pop());
+            }
+            return OutSt.peek();
+        }
+        else
+        {
+            return -1;
+        }
+    }
+    
+    public boolean empty() {
+        return OutSt.isEmpty()&& InSt.isEmpty();
+    }
+}
+// Problem 2: Design Hashmap (https://leetcode.com/problems/design-hashmap/)
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+
+// Time Complexity : Amortized time complexity O(1)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : yes - a few compliation errors due to typos.
+
+
+// Your code here along with comments explaining your approach
+
