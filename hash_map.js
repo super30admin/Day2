fb314a636fb2d26e8cc3e4c2d2004ffd671dbfd2
Malav Soni <malav@live.com>
2024-10-23 22:45:30 +0530

    Design 2 Completed

---
+++ hash_map.js
@@ -0,0 +1,92 @@
+// Time Complexity : put, get, remove O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+// Your code here along with comments explaining your approach
+
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+  }
+}
+
+var MyHashMap = function () {
+  this.bucket = 10000;
+  this.storage = new Array(this.bucket).fill(null);
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function (key, value) {
+  let primaryIndex = this.getPrimaryHash(key);
+  if (this.storage[primaryIndex] == null) {
+    this.storage[primaryIndex] = new Node(-1, -1);
+  }
+  let previousNode = this.getPreviousNode(this.storage[primaryIndex], key);
+  if (previousNode.next == null) {
+    previousNode.next = new Node(key, value);
+  } else {
+    previousNode.next.value = value;
+  }
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function (key) {
+  let primaryIndex = this.getPrimaryHash(key);
+  if (this.storage[primaryIndex] == null) {
+    return -1;
+  }
+  let previousNode = this.getPreviousNode(this.storage[primaryIndex], key);
+  if (previousNode.next == null) {
+    return -1;
+  } else {
+    return previousNode.next.value;
+  }
+};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function (key) {
+  let primaryIndex = this.getPrimaryHash(key);
+  if (this.storage[primaryIndex] == null) {
+    this.storage[primaryIndex] = new Node(-1, -1);
+  }
+  let previousNode = this.getPreviousNode(this.storage[primaryIndex], key);
+  if (previousNode.next == null) {
+    return -1;
+  } else {
+    previousNode.next = previousNode.next.next;
+  }
+};
+
+MyHashMap.prototype.getPreviousNode = function (node, key) {
+  previousNode = null;
+  currentNode = node;
+  while (currentNode != null && currentNode.key != key) {
+    previousNode = currentNode;
+    currentNode = currentNode.next;
+  }
+  return previousNode;
+};
+
+MyHashMap.prototype.getPrimaryHash = function (key) {
+  return key % this.bucket;
+};
+
+//  * Your MyHashMap object will be instantiated and called as such:
+var obj = new MyHashMap();
+obj.put(1, 1);
+obj.put(2, 2);
+console.log(obj.get(1));
+obj.remove(5);
---
+++ queue_with_stack.js
@@ -0,0 +1,62 @@
+// Time Complexity : Push, Pop, Peek O(1)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+
+// Your code here along with comments explaining your approach
+
+var MyQueue = function () {
+  this.in_stack = [];
+  this.out_stack = [];
+};
+
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function (x) {
+  this.in_stack.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function () {
+  if (this.out_stack.length == 0) {
+    this.transfer();
+  }
+  return this.out_stack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function () {
+  if (this.out_stack.length == 0) {
+    this.transfer();
+  }
+  return this.out_stack[this.out_stack.length - 1];
+};
+
+MyQueue.prototype.transfer = function () {
+  while (this.in_stack.length != 0) {
+    this.out_stack.push(this.in_stack.pop());
+  }
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function () {
+  return this.in_stack.length == 0 && this.out_stack.length == 0;
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
