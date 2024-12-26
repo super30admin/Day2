95d374e99766c10b06a6d5e745064095cad08446
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2024-12-25 18:21:49 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,34 @@
 
 
 // Your code here along with comments explaining your approach
+class MyQueue {
+    Stack<Integer> input;
+    Stack<Integer> output;
+    public MyQueue() {
+        input = new Stack<Integer>();
+        output = new Stack<Integer>();
+    }
+    
+    public void push(int x) {
+        input.push(x);
+    }
+    
+    public int pop() {
+        peek();
+        return output.pop();
+    }
+
+    //Need to do this in peek() and not in pop() as it will remove the element, to have common code usage
+    public int peek() {
+        if(output.empty()) {
+            while(!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+    
+    public boolean empty() {
+        return output.empty() && input.empty();
+    }
+}
