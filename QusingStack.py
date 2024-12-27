46b1a4ef110e686308448fbbef53523682413c02
vallarirajurkar <vallarirajurkar@csu.fullerton.edu>
2024-12-27 00:41:52 -0800

    Completed Queue using stack and designing hashmap

--- QusingStack.py
+++ QusingStack.py
@@ -1,3 +1,12 @@
+''' 
+ Time Complexity : Ammortized O(1)
+ Space Complexity : O(n)
+ Did this code successfully run on Leetcode : Yes
+ Any problem you faced while coding this : Difficulty in analyzing the second method: shift elements while pushing
+
+
+Your code here along with comments explaining your approach
+'''
 class MyQueue:
 
     def __init__(self):
