fea7d1980aa96919bd60500a176eabf4967d2d53
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2024-12-26 17:26:50 -0800

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -36,3 +36,90 @@ class MyQueue {
         return output.empty() && input.empty();
     }
 }
+
+
+
+class MyHashMap {
+
+    class Node {
+        int key,value;
+        Node next;
+        Node(int key,int value) {
+            this.key = key;
+            this.value= value;
+        }
+    }
+    Node[] storage;
+    Node dummy=null, prev=null, curr=null;
+
+    int getBucket(int key) {
+        return key%10000;
+    }
+
+    public MyHashMap() {
+        storage = new Node[10000];
+    }
+
+    public void find(int key, int index) {
+        dummy=storage[index];
+        prev=dummy;
+        curr = dummy.next;
+        while(curr != null && curr.key != key) {
+            prev=curr;
+            curr=curr.next;
+        }
+    }
+    
+    public void put(int key, int value) {
+        int index= getBucket(key);
+        if (storage[index] == null) {
+            dummy= new Node(-1,-1);
+            dummy.next = new Node(key,value);
+            storage[index] = dummy;
+        } else {
+            //traverse the list to check if key exists or not.
+            find(key,index);
+            if(prev.next == null) {
+                prev.next = new Node(key, value);
+            } else {
+                prev.next.value=value;
+            }
+        }
+
+    }
+
+    
+    public int get(int key) {
+        int index = getBucket(key) ;
+        if (storage[index] == null) {
+            return -1;
+        } else {
+            find(key, index);
+            if(prev.next == null) {
+                return -1;
+            } 
+        }
+        return prev.next.value;
+    }
+    
+    public void remove(int key) {
+        int index = getBucket(key);
+        if(storage[index] == null) {
+            return;
+        } else {
+            find(key,index);
+            if(prev.next != null) {
+                prev.next = prev.next.next;
+            }
+        }
+        return;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
