deb0b5ab9f77bb05b6c4e5ef3c38f9cbf0cc065a
Monisha R <rajumonisha024@gmail.com>
2025-01-24 09:20:47 -0500

    Design 2 - completed problem 1

---
+++ queue_using_stacks.py
@@ -0,0 +1,27 @@
+# Time Complexity : amortized O(1)
+# Space Complexity : O(n)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : none
+
+class MyQueue:
+
+    def __init__(self):
+        self.in_stack = []
+        self.out_stack = []
+
+    def push(self, x: int) -> None:
+        self.in_stack.append(x)
+
+    def pop(self) -> int:
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack.pop()
+
+    def peek(self) -> int:
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        return self.out_stack[-1]       
+    def empty(self) -> bool:
+        return not self.out_stack and not self.in_stack
