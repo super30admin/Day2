d0b11866d2e9c8d45889425fcb2d10951c6ced70
Rachana <S565730@nwmissouri.edu>
2025-01-27 14:00:14 -0500

    done

--- Sample.java
+++ Sample.java
@@ -1,7 +1,24 @@
 // Time Complexity :
 // Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Did this code successfully run on Leetcode :no
+// Any problem you faced while coding this :no
+
+
+
+
+
+//problme 1
+//Time Complexity:
+//push: 𝑂(1)
+//pop: o(1) (amortized)
+//peek: o(1) (amortized)
+//Space Complexity: o(n).
+
+//problem 2:
+// Time Complexity :o(1)
+// Space Complexity :o(1)
+
+
 
 
 // Your code here along with comments explaining your approach
